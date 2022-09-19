0ee4fb485850d669e5ecac1b83eccc4086a183fe
Vhsz <vhsz4@mail.ru>
2022-09-19 14:40:32 +0400

    first-commit

---
+++ TestCalc/CalculTests.cs
@@ -0,0 +1,22 @@
+﻿using Calc;
+using System;
+using Xunit;
+
+namespace TestCalc
+{
+    public class CalculTests
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            // Arrange
+            var calcul = Calcul.Compute(6,7,'+');
+
+            // Act
+
+
+            // Assert
+            Assert.Equal(13,calcul);
+        }
+    }
+}
