30405afc1ab4d745cef20502f545ac896f587704
Vhsz <vhsz4@mail.ru>
2022-09-27 21:07:23 +0400

    add 4-unit test and buildTest.bat

--- TestCalc/CalculTests.cs
+++ TestCalc/CalculTests.cs
@@ -10,13 +10,36 @@ namespace TestCalc
         public void TestMethod1()
         {
             // Arrange
-            var calcul = Calcul.Compute(6,7,'+');
-
-            // Act
-
-
+            var calcul = Calcul.Compute(6, 7, '+');
             // Assert
             Assert.Equal(13,calcul);
+          
+        }
+
+        [Fact]
+        public void TestMethod2()
+        {
+            // Arrange
+            var calcul = Calcul.Compute(6, 7, '+');
+            var calcul1 = Calcul.Compute(7, 7, '-');
+         
+            Assert.Equal(13, calcul);
+            Assert.Equal(0, calcul1);
+          
+        }
+
+        [Fact]
+        public void TestMethod3()
+        {
+            var calcul2 = Calcul.Compute(8, 8, '*');
+            Assert.Equal(64, calcul2);
+        }
+
+        [Fact]
+        public void TestMethod4()
+        {
+            var calcul3 = Calcul.Compute(8, 8, '/');
+            Assert.Equal(1, calcul3);
         }
     }
 }
--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 dotnet restore
 dotnet build --no-restore 
-dotnet test --no-build --verbosity normal 
+
