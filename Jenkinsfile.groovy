347152ae09938fe1e0037ddbea67ec7a4e6893ae
Vhsz <113422401+Vhsz@users.noreply.github.com>
2022-09-27 22:07:35 +0400

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -22,8 +22,6 @@ pipeline {
                         } catch (Exception e){}
                     }
                     echo "Current build: ${BUILD_NUMBER}"
-                    zip zipFile: "artifacts\\${BUILD_NUBMER}.zip", archive:fasle,
-                        dir: 'C:\\Users\\Alex\\AppData\\Local\\Jenkins\\.jenkins\\workspace\\TestJankins'
                     archiveArtifacts artifacts: "artifacts\\${BUILD_NUMBER}.zip"
             }
         }
