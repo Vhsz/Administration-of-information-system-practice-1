8523100669898a73031df120b4d172ce166c2788
Vhsz <113422401+Vhsz@users.noreply.github.com>
2022-09-28 14:38:36 +0400

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -22,7 +22,7 @@ pipeline {
     }
         post {
             always{
-                emailext attachLog: true, body: '''$PROJECT_NAME - Build # $BUILD_NUBMER - $BUILD_STATUS:
+                emailext attachLog: true, body: '''$PROJECT_NAME - Build # $BUILD_NUMBER - $BUILD_STATUS:
                 Check console output at $BUILD_URL to view the results.''', subject: '$PROJECT_NAME - Build # $BUILD_NUMBER - $BUILD_STATUS!',
                         to: 'arg0ncmd@gmail.com'
             }
