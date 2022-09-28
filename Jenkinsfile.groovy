caea2fbced6af174ed034f0e66953610f21c79e3
Vhsz <113422401+Vhsz@users.noreply.github.com>
2022-09-28 13:59:05 +0400

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -15,7 +15,7 @@ pipeline {
        stage('Archive') {
             steps {
                     echo "Current build: ${BUILD_NUMBER}"
-                    zip zipFile: "artifacts\\${BUILD_NUMBER}.zip, archive:fasle, dir: 'C:\\Users\\Alex\\AppData\\Local\\Jenkins\\.jenkins\\workspace\\TestJankins\\\Calc\\bin\\Debug\\netcoreapp3.1'
+                    zip zipFile: "artifacts\\${BUILD_NUMBER}.zip, archive:fasle, dir: 'C:\\Users\\Alex\\AppData\\Local\\Jenkins\\.jenkins\\workspace\\TestJankins\\Calc\\bin\\Debug\\netcoreapp3.1'
                     archiveArtifacts artifacts: "artifacts\\${BUILD_NUMBER}.zip"
             }
         }
