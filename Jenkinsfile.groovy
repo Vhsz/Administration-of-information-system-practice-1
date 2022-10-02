592df88785616c714546f2c2aa0b559f123cae9f
Vhsz <113422401+Vhsz@users.noreply.github.com>
2022-10-02 21:22:05 +0400

    add cleanup

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -42,6 +42,9 @@ pipeline {
             Check console output at $BUILD_URL to view the results.''', subject: '$PROJECT_NAME - Build # $BUILD_NUMBER - $BUILD_STATUS!',
                 to: 'kocherginalekseysstu@gmail.com'
             }
+	cleanup {
+              cleanWs()
+          }
         }
     }
 
