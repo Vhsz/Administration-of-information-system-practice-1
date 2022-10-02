c2706e7f1c11e1e9bf2a560ee57f8e623830dcba
Vhsz <113422401+Vhsz@users.noreply.github.com>
2022-10-02 19:07:43 +0400

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -22,16 +22,5 @@ pipeline {
                     archiveArtifacts artifacts: "${BUILD_NUMBER}.zip"
 			}
 		  }
-    }
-
-       
-      
-     post {
-        always{
-            emailext attachLog: true, body: '''$PROJECT_NAME - Build # $BUILD_NUMBER - $BUILD_STATUS:
-            Check console output at $BUILD_URL to view the results.''', subject: '$PROJECT_NAME - Build # $BUILD_NUMBER - $BUILD_STATUS!',
-                to: 'kocherginalekseysstu@gmail.com'
-            }
-        }
-    }
+    	}
 }
