cfa86738fa9d0ffd7c16ca9abd7d25aad0aed181
Vhsz <113422401+Vhsz@users.noreply.github.com>
2022-10-03 21:14:38 +0400

    add name test

--- TestCalc/CalculTests.cs
+++ TestCalc/CalculTests.cs
@@ -7,37 +7,28 @@ namespace TestCalc
     public class CalculTests
     {
         [Fact]
-        public void TestMethod1()
+        public void Amount()
         {
-            // Arrange
             var calcul = Calcul.Compute(6, 7, '+');
-            // Assert
             Assert.Equal(13,calcul);
-          
         }
         
         [Fact]
-        public void TestMethod2()
+        public void Subtraction()
         {
-            // Arrange
-            var calcul = Calcul.Compute(6, 7, '+');
             var calcul1 = Calcul.Compute(7, 7, '-');
-
-            Assert.Equal(13, calcul);
             Assert.Equal(0, calcul1);
-
         }
         
          [Fact]
-        public void TestMethod3()
+        public void Multiplication()
         {
             var calcul2 = Calcul.Compute(8, 8, '*');
             Assert.Equal(64, calcul2);
         }
-
         
         [Fact]
-        public void TestMethod4()
+        public void Division ()
         {
             var calcul3 = Calcul.Compute(8, 8, '/');
             Assert.Equal(1, calcul3);
